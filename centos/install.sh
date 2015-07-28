67ac405256b17c69a34cdfc6d1dacbca9e0c37f3
hlee13 <55885293@qq.com>
2015-07-29 03:21:39 +0800

    init centos

---
+++ centos/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# File    : install.sh
+# Author  : lihan
+# Version : 1.0
+# Company : xiaojukeji
+# Contact : lihan@diditaxi.com.cn
+# Date    : 2015-07-29 03:17:57
+
+ls *.rpm | xargs rpm -i --force 
+
